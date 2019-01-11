a8593df99b5bbd6fa786565bf56fdeb1a237f872
Zhuang jy <526223908@qq.com>
2019-01-11 19:49:45 +0800

    购物车实现1

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,8 @@
 //index.js
 //获取应用实例
 var app = getApp()
+
+var cart_list = [];
 Page({
   data: {
     status: 0,
@@ -27,6 +29,38 @@ Page({
       wx.hideLoading();
     },1000)
   },
+
+  AddToCart: function (event) {
+    wx.showToast({
+      title: '购物车+1',
+      icon: 'succes',
+      duration: 500,
+      mask: true
+    })
+    var foodname = event.currentTarget.dataset.foodname;
+    var picsource = event.currentTarget.dataset.picsource;
+    var price = event.currentTarget.dataset.price;
+    var num = 1;
+    var flag = 0;
+    for (var x in cart_list)
+    {
+      if (cart_list[x][0] == foodname) { 
+        cart_list[x][3]+=1;
+        flag = 1;
+      }
+    }
+    if(flag==0){
+      cart_list.push([foodname, picsource, price, num])
+    }
+    for (var i in cart_list) {
+      console.log(i + "-----" + cart_list[i]);
+    }
+    
+
+  },
+
+
+
   previewImages: function () {
     wx.previewImage({
       current: 'http://p1.meituan.net/460.280/deal/5cae86dd953bc50457aea6219e85287d79414.jpg@460w_280h_1e_1c',
