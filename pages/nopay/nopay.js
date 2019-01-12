3a1d55595996c4217be6e453e1c588884e2b3d85
Zhuang jy <526223908@qq.com>
2019-01-12 14:38:01 +0800

    密码输入3

--- pages/nopay/nopay.js
+++ pages/nopay/nopay.js
@@ -3,103 +3,31 @@ var app = getApp()
 
 Page({
   data: {
+    userPwd: "1",
     timeCounter: null,
     postList: app.globalData.local_database,
     total_price: 0,
     random_num:0,
     showModalStatus: false ,
-    Length: 6,    //输入框个数 
-    isFocus: true,  //聚焦 
-    Value: "",    //输入的内容 
-    ispassword: true, //是否密文显示 true为密文， false为明文。
   },
 
-  Focus(e) {
+  passWdInput: function (e) {
     var that = this;
-    console.log(e.detail.value);
-    var inputValue = e.detail.value;
+    var tmp = e.detail.value;
+    
+    console.log(tmp);
     that.setData({
-      Value: inputValue,
-    })
-  },
-  Tap() {
-    var that = this;
-    that.setData({
-      isFocus: true,
-    })
-  },
-  formSubmit(e) {
-    console.log(e.detail.value.password);
-  }, 
-  powerDrawer: function (e) {
-    var currentStatu = e.currentTarget.dataset.statu;
-    this.util(currentStatu)
-  },
-  util: function (currentStatu) {
-    /* 动画部分 */
-    // 第1步：创建动画实例   
-    var animation = wx.createAnimation({
-      duration: 200,  //动画时长  
-      timingFunction: "linear", //线性  
-      delay: 0  //0则不延迟  
+      userPwd: tmp
     });
-
-    // 第2步：这个动画实例赋给当前的动画实例  
-    this.animation = animation;
-
-    // 第3步：执行第一组动画  
-    animation.opacity(0).rotateX(-100).step();
-
-    // 第4步：导出动画对象赋给数据对象储存  
-    this.setData({
-      animationData: animation.export()
-    })
-
-    // 第5步：设置定时器到指定时候后，执行第二组动画  
-    setTimeout(function () {
-      // 执行第二组动画  
-      animation.opacity(1).rotateX(0).step();
-      // 给数据对象储存的第一组动画，更替为执行完第二组动画的动画对象  
-      this.setData({
-        animationData: animation
-      })
-
-      //关闭  
-      if (currentStatu == "close") {
-        this.setData(
-          {
-            showModalStatus: false
-          }
-        );
-      }
-    }.bind(this), 200)
-
-    // 显示  
-    if (currentStatu == "open") {
-      this.setData(
-        {
-          showModalStatus: true
-        }
-      );
-    }
-  } ,
-
-  goto_logs: function () {
-    var random = Math.floor(Math.random() * (99999999 - 10000000 + 1) + 10000000);
-    this.setData({
-      random_num: random
-    })
-
   },
 
-
-  calculate_sun: function () {
-    var sum = 0;
-    for (var x in app.globalData.local_database) {
-      sum += app.globalData.local_database[x].price * 1.0 * app.globalData.local_database[x].num * 1.0
-    }
-    return sum;
+  submittoserver: function(){
+    var currentStatu = "close";
+    this.util(currentStatu);
+    console.log(this.total_price);
+    console.log(this.userPwd);
   },
+ 
 
   
 
@@ -159,3 +87,73 @@ Page({
   },
 
 })
+
+powerDrawer: function (e) {
+  var currentStatu = e.currentTarget.dataset.statu;
+  this.util(currentStatu)
+},
+util: function (currentStatu) {
+  /* 动画部分 */
+  // 第1步：创建动画实例   
+  var animation = wx.createAnimation({
+    duration: 200,  //动画时长  
+    timingFunction: "linear", //线性  
+    delay: 0  //0则不延迟  
+  });
+
+  // 第2步：这个动画实例赋给当前的动画实例  
+  this.animation = animation;
+
+  // 第3步：执行第一组动画  
+  animation.opacity(0).rotateX(-100).step();
+
+  // 第4步：导出动画对象赋给数据对象储存  
+  this.setData({
+    animationData: animation.export()
+  })
+
+  // 第5步：设置定时器到指定时候后，执行第二组动画  
+  setTimeout(function () {
+    // 执行第二组动画  
+    animation.opacity(1).rotateX(0).step();
+    // 给数据对象储存的第一组动画，更替为执行完第二组动画的动画对象  
+    this.setData({
+      animationData: animation
+    })
+
+    //关闭  
+    if (currentStatu == "close") {
+      this.setData(
+        {
+          showModalStatus: false
+        }
+      );
+    }
+  }.bind(this), 200)
+
+  // 显示  
+  if (currentStatu == "open") {
+    this.setData(
+      {
+        showModalStatus: true
+      }
+    );
+  }
+} ,
+
+goto_logs: function () {
+  var random = Math.floor(Math.random() * (99999999 - 10000000 + 1) + 10000000);
+  this.setData({
+    random_num: random
+  })
+
+},
+
+
+calculate_sun: function () {
+  var sum = 0;
+  for (var x in app.globalData.local_database) {
+    sum += app.globalData.local_database[x].price * 1.0 * app.globalData.local_database[x].num * 1.0
+  }
+  return sum;
+},
